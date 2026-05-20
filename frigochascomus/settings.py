953e7e5a972b4ca335ed6995085720da07b57f10
Lautaro <lautidelamano@gmail.com>
2026-05-20 16:51:56 -0300

    cambio de proveedor modificaciones email

--- frigochascomus/settings.py
+++ frigochascomus/settings.py
@@ -72,7 +72,6 @@ EMAIL_HOST       = 'smtp-relay.brevo.com'
 EMAIL_PORT       = 587
 EMAIL_USE_TLS    = False
 EMAIL_USE_SSL = True
-EMAIL_TIMEOUT = 10
 EMAIL_HOST_USER  = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL  = 'Frigo Chascomús <chascomusfrigo@gmail.com>'
