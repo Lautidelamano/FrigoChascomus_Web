55761a0aa821a6956ec90c78ae84f6cee930ede6
Lautaro <lautidelamano@gmail.com>
2026-05-20 17:06:49 -0300

    modificaciones settings

--- frigochascomus/settings.py
+++ frigochascomus/settings.py
@@ -109,6 +109,3 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/admin/login/'
-
-print("EMAIL USER:", EMAIL_HOST_USER)
-print("EMAIL PASS:", EMAIL_HOST_PASSWORD)
