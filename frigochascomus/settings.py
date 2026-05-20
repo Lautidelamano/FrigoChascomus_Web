ed318b8904cb1225e7684b61c7166cb99191418c
Lautaro <lautidelamano@gmail.com>
2026-05-20 16:42:23 -0300

    Cambio mails de gmail a brevo

--- frigochascomus/settings.py
+++ frigochascomus/settings.py
@@ -68,7 +68,7 @@ else:
 
 # Email
 EMAIL_BACKEND    = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST       = 'smtp.gmail.com'
+EMAIL_HOST       = 'smtp-relay.brevo.com'
 EMAIL_PORT       = 587
 EMAIL_USE_TLS    = False
 EMAIL_USE_SSL = True
